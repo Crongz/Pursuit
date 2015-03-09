03cfa294f59d36b19942e822a9cd9cdbe6a51cd3
george <ruan3@illinois.edu>
2015-03-08 23:08:16 -0500

    Add some data class variables, initializer, get, and set methods

--- Pursuit/Data.swift
+++ Pursuit/Data.swift
@@ -8,26 +8,158 @@
 
 import Foundation
 
-class Data {
+public class Data {
     
-    /* Class Variables */
-    var username:String?
-    var password:String?
-    var firstName:String?
-    var lastName:String?
+    /* Public
+    ========================================*/
     
-    var gpa:String?
-    var major:String?
-    var degree:String?
-    var skills:[String]?
-    var matches:[String]?
+    /* Get Functions 
+    ----------------------------------------*/
+    public func getUsername() -> String
+    {
+        return username!
+    }
     
-    var radius:Double?
-    var payRange:Double?
+    public func getPassword() -> String
+    {
+        return password!
+    }
     
-    /* Initializer */
+    public func getFirstName() -> String
+    {
+        return firstName!
+    }
+    
+    public func getlastName() -> String
+    {
+        return lastName!
+    }
+    
+    public func getGpa() -> Double
+    {
+        return gpa!
+    }
+    
+    public func getMajor() -> String
+    {
+        return major!
+    }
+    
+    public func getDegree() -> String
+    {
+        return degree!
+    }
+    
+    public func getSkills() -> [String]
+    {
+        return skills!
+    }
+    
+    public func getMatches() -> [String]
+    {
+        return matches!
+    }
+    
+    public func getRadius() -> Double
+    {
+        return radius!
+    }
+    
+    public func getPayRange() -> Double
+    {
+        return payRange!
+    }
+    
+    /* Set Functions 
+    ----------------------------------------*/
+    public func setUsername(username:String)
+    {
+        return self.username = username
+    }
+    
+    public func setPassword(password:String)
+    {
+        return self.password = password
+    }
+    
+    public func setFirstName(firstName:String)
+    {
+        return self.firstName = firstName
+    }
+    
+    public func setLastName(lastName:String)
+    {
+        return self.lastName = lastName
+    }
+    
+    public func setGpa(gpa:Double)
+    {
+        return self.gpa = gpa
+    }
+    
+    public func setMajor(major:String)
+    {
+        return self.major = major
+    }
+    
+    public func setDegree(degree:String)
+    {
+        return self.degree = degree
+    }
+    
+    public func setSkills(skills:[String])
+    {
+        return self.skills = skills
+    }
+    
+    public func setMatches(matches:[String])
+    {
+        return self.matches = matches
+    }
+    
+    public func setRadius(radius:Double)
+    {
+        return self.radius = radius
+    }
+    
+    public func setPayRange(payRange:Double)
+    {
+        return self.payRange = payRange
+    }
+    
+    /* Private 
+    ========================================*/
+    
+   
+    /* Class Variables
+    ----------------------------------------*/
+    
+    private var username:String?
+    private var password:String?
+    private var firstName:String?
+    private var lastName:String?
+    
+    private var gpa:Double?
+    private var major:String?
+    private var degree:String?
+    private var skills:[String]?
+    private var matches:[String]?
+    
+    private var radius:Double?
+    private var payRange:Double?
+    
+    /*!! Any variables past this list need an public GET and SET method to call !! */
+    
+    /* Initializers
+    ----------------------------------------*/
+    
+    /* Default */
+    init() {}
+    
+    /* Initialize values as well 
+     * Not sure if we need this one */
     init(username: String, password:String, firstName:String, lastName:String,
-        gpa:String, major:String, degree:String, skills:[String], matches:[String],
+        gpa:Double, major:String, degree:String, skills:[String], matches:[String],
         radius:Double, payRange:Double)
     {
         self.username = username
