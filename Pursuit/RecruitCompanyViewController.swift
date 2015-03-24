22d662c54d4331ba747447e632f3bb7ce2bbafee
Crongz <leejamesws@gmail.com>
2015-03-24 01:11:16 -0500

    Working on Recruiters sign up -James Lee

---
+++ Pursuit/RecruitCompanyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecruitCompanyViewController.swift
+//  Pursuit
+//
+//  Created by James Lee on 3/23/15.
+//  Copyright (c) 2015 Pursuit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+
+class RecruitCompanyViewController: UITableViewController {
+    
+
+    
+}
